777b1949d9a03b285eea885cfc76c39f1e2a7ea5
Effortless Mountain <effortlessmountain@gmail.com>
2019-11-29 20:14:33 -0800

    get svgs to show in download and reposition before converting for more consistent image

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 
     const bonded = {
         type: "path",
-        icon: "◊",
+        icon: {
+            type: "svg",
+            author: "Delapouite",
+            name: "Burning forest icon",
+            svg: '<svg style="height: 512px; width: 512px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g class="" style="touch-action: none;" transform="translate(0,0)"><path d="M281.3 18.28c-39.1 33.39 21.8 67.98-4.6 77.4-27.1 9.72-48.6-22.86-62.7-55.79-26.6 23.91-38.4 76.01-36.1 116.71-21.7-8.7-25.3-18.3-23.3-48.8-71.2 40.5 11 124.2-39.3 133.3-17.77 3.2-52.61-17-56.65-48.5C29.39 235.2 44.37 294.8 81.64 344c-32.5 5.7-45.4-9.9-56.81-23.5-20.829 60.7 4.9 104.4 53.83 132.3 28.44-23 58.24-44 79.84-78.5-24.8 9.6-38 4.5-60.3 5.2 30-26.5 57.4-61 77.4-92-22.8 6.7-31 11.4-50.5 4.9 23.4-19.8 44.4-39.4 59.6-66.2-8 5.8-18.3 8.6-33.1 5 26.7-22 40.6-33.2 53.2-63.7 6 22.5 15 42.6 37.9 64-8.1 2.6-17.4-.1-31.4-4.9 13.8 22.3 41.7 47.9 69.1 64.5-15.6 5.7-35.1 4.1-52.9-2.5 12.6 33.4 52.3 59.9 78.9 88.1-20.5 6.6-43.2.6-62.6-1.4 13.7 27 40.2 45.8 66.4 64.9 11.2-8.2 21.3-17.1 27.5-28.3-13.2 1.2-28.7 4.9-42.7.8 18.1-17.6 46.4-31.6 55-52.4-12.1 4.1-26.6 4.3-37.3.8 18.7-10.4 35.9-28.8 45.3-42.7-9.5 3-15.2 3.9-23.2 3.1 12.8-10.7 27.3-25.9 31.3-40 10.6 13.2 16.2 26.4 34.4 40.2-10.1 2.3-16.6.2-22.6-1.5 10.4 16.7 35 32.6 50.9 45-15.9-.3-28.7.8-44.7-8.3 13.7 19.4 32.3 41 52.8 57.5-14.7 3.8-20.8 2-37.7-4 10.9 15.9 22.9 27.7 36.3 38.6 41.3-30.7 59.3-79.6 57.4-150.2-16 33.3-37.3 31.8-58.7 27.7 17.3-10.9 40.3-100.3 24.9-123.4-4.2 31.1-45.1 87.3-74.1 61.5 44.6-61.8 43-88.3 24.6-121.5-9.6 20-25.3 64.9-57.3 53.5 62.7-110.89-55.7-119.16-71-178.32zM80.12 453.6c28.88 16.2 65.68 26.9 105.68 32.5l2.1-16.7c-35.5-1.9-71.5-7.8-107.78-15.8zm209.58 7.2c-24 5.9-48.4 8.7-73.1 9.1l1.3 19.5c11.5.9 23.1 1.3 34.8 1.3 43.3.1 80.2-3.2 111.2-10.5l.3-9.2c-25.4.3-50.3-2.5-74.5-10.2zm110.2 8l-2.7.3.2.7c.8-.3 1.7-.7 2.5-1z" fill="#fff" fill-opacity="1"></path></g></svg>'
+        },
         name: "Bonded",
         abilities: [
             {
@@ -46,7 +51,12 @@
 
     const caveLion = {
         type: "companion",
-        icon: "C",
+        icon: {
+            "type": "svg",
+            "author": "Delapouite",
+            "name": "Burning forest icon",
+            "svg": "<svg style=\"height: 512px; width: 512px;\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><g class=\"\" style=\"touch-action: none;\" transform=\"translate(0,0)\"><path d=\"M281.3 18.28c-39.1 33.39 21.8 67.98-4.6 77.4-27.1 9.72-48.6-22.86-62.7-55.79-26.6 23.91-38.4 76.01-36.1 116.71-21.7-8.7-25.3-18.3-23.3-48.8-71.2 40.5 11 124.2-39.3 133.3-17.77 3.2-52.61-17-56.65-48.5C29.39 235.2 44.37 294.8 81.64 344c-32.5 5.7-45.4-9.9-56.81-23.5-20.829 60.7 4.9 104.4 53.83 132.3 28.44-23 58.24-44 79.84-78.5-24.8 9.6-38 4.5-60.3 5.2 30-26.5 57.4-61 77.4-92-22.8 6.7-31 11.4-50.5 4.9 23.4-19.8 44.4-39.4 59.6-66.2-8 5.8-18.3 8.6-33.1 5 26.7-22 40.6-33.2 53.2-63.7 6 22.5 15 42.6 37.9 64-8.1 2.6-17.4-.1-31.4-4.9 13.8 22.3 41.7 47.9 69.1 64.5-15.6 5.7-35.1 4.1-52.9-2.5 12.6 33.4 52.3 59.9 78.9 88.1-20.5 6.6-43.2.6-62.6-1.4 13.7 27 40.2 45.8 66.4 64.9 11.2-8.2 21.3-17.1 27.5-28.3-13.2 1.2-28.7 4.9-42.7.8 18.1-17.6 46.4-31.6 55-52.4-12.1 4.1-26.6 4.3-37.3.8 18.7-10.4 35.9-28.8 45.3-42.7-9.5 3-15.2 3.9-23.2 3.1 12.8-10.7 27.3-25.9 31.3-40 10.6 13.2 16.2 26.4 34.4 40.2-10.1 2.3-16.6.2-22.6-1.5 10.4 16.7 35 32.6 50.9 45-15.9-.3-28.7.8-44.7-8.3 13.7 19.4 32.3 41 52.8 57.5-14.7 3.8-20.8 2-37.7-4 10.9 15.9 22.9 27.7 36.3 38.6 41.3-30.7 59.3-79.6 57.4-150.2-16 33.3-37.3 31.8-58.7 27.7 17.3-10.9 40.3-100.3 24.9-123.4-4.2 31.1-45.1 87.3-74.1 61.5 44.6-61.8 43-88.3 24.6-121.5-9.6 20-25.3 64.9-57.3 53.5 62.7-110.89-55.7-119.16-71-178.32zM80.12 453.6c28.88 16.2 65.68 26.9 105.68 32.5l2.1-16.7c-35.5-1.9-71.5-7.8-107.78-15.8zm209.58 7.2c-24 5.9-48.4 8.7-73.1 9.1l1.3 19.5c11.5.9 23.1 1.3 34.8 1.3 43.3.1 80.2-3.2 111.2-10.5l.3-9.2c-25.4.3-50.3-2.5-74.5-10.2zm110.2 8l-2.7.3.2.7c.8-.3 1.7-.7 2.5-1z\" fill=\"#fff\" fill-opacity=\"1\"></path></g></svg>"
+        },
         name: "Cave Lion",
         writeIn: "Name",
         track: 4,
@@ -71,7 +81,7 @@
     }
 
     const calculateScale = () => {
-        if (window.innerHeight > 1100) {
+        if (window.innerHeight > 1070) {
             return "full"
         } else if (window.innerHeight > 750) {
             return "two-thirds"
@@ -105,9 +115,11 @@
 
     const createTrackValueHtml = (track, value) => {
         if (value > track) {
-            return `<div class="value empty"></div>`
+            return `<div class="value empty">
+                        <svg xmlns='http://www.w3.org/2000/svg' version='1.1' preserveAspectRatio='none' viewBox='0 0 10 10'><line x1='0' y1='10' x2='10' y2='0' stroke='black' vector-effect='non-scaling-stroke' style='stroke:rgb(65,64,66);stroke-width:4' /></svg>
+                    </div>`
         } else {
-            return `<div class="value">${value}</div>`
+            return `<div class="value number">+${value}</div>`
         }
     }
 
@@ -116,7 +128,7 @@
             return ""
         } else {
             return `<div class="track">
-                    <div class="value zero">0</div>
+                    <div class="value first">0</div>
                     ${createTrackValueHtml(track, 1)}
                     ${createTrackValueHtml(track, 2)}
                     ${createTrackValueHtml(track, 3)}
@@ -126,13 +138,20 @@
         }
     }
 
+    const createIcon = (icon) => {
+        if (typeof icon === "string") {
+            return `<i class="header-icon">${icon}</i>`
+        } else {
+            return `${icon.svg}`
+        }
+    }
     const createAssetHtml = (asset = {}, scale = "full") => {
         return `<div class="asset ${scale}">
                 <div class="main-matter">
                     <div class="top">
                         <div class="type">${asset.type}</div>
                         <div class="header-circle">
-                            <i class="header-icon">${asset.icon}</i>
+                            ${createIcon(asset.icon)}
                         </div>
                         <div class="asset-name">${asset.name}</div>
                     </div>
@@ -162,6 +181,7 @@
     const showSingleAssetExample = (asset) => {
         assetInput.value = JSON.stringify(asset, null, 2)
         assetContainer.innerHTML = createAssetHtml(asset, window.IAW_scale)
+        setSvgDimensions()
     }
 
     const showScreen = (screen) => {
@@ -169,6 +189,8 @@
             document.body.className = "download-screen"
         } else if (screen === "main") {
             document.body.className = "main-screen"
+        } else if (screen = "render") {
+            document.body.className = "render"
         }
     }
 
@@ -181,11 +203,24 @@
         document.body.removeChild(link)
     }
 
+    const setSvgDimensions = () => {
+        const svgs = document.querySelectorAll('svg')
+        svgs.forEach(svg => {
+            svg.setAttribute('height', svg.parentNode.offsetHeight)
+            svg.setAttribute('width', svg.parentNode.offsetWidth)
+        })
+    }
+
     const screenshot = () => {
+        const render = document.querySelector('.render')
         const asset = document.querySelector('.asset')
-        // asset.setAttribute("style", "width:375px;height:525px;font-size:1.1em")
+        render.appendChild(asset)
+        showScreen('render')
+        window.scrollTo(0, 0)
+        setSvgDimensions()
         html2canvas(asset,
             {
+                allowTaint: true
                 // width: "750",
                 // height: "1050",
                 // scale: 2
@@ -195,6 +230,7 @@
                 downloadContainer.appendChild(canvas)
                 // saveImage(canvas.toDataURL(), 'asset.png') //TODO name after asset's name
                 showScreen('download')
+                document.querySelector('.assets').appendChild(asset)
             })
     }
 
@@ -210,6 +246,7 @@
         } else {
             assetContainer.innerHTML = createAssetHtml(data, window.IAW_scale)
         }
+        setSvgDimensions()
     }
 
 
