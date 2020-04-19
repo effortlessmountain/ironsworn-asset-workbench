b5d89867a7febab5f18910e07789348a7d664e54
Effortless Mountain <effortlessmountain@gmail.com>
2020-04-19 16:26:32 -0700

    Updates

---
+++ precache-manifest.d38df8ca14f4fe1217e7c4d2f70762c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b007560cdeb03102414d84782885cf9",
+    "url": "/ironsworn-asset-workbench/index.html"
+  },
+  {
+    "revision": "fb0650293597c212aeb4",
+    "url": "/ironsworn-asset-workbench/static/css/main.e24d06d0.chunk.css"
+  },
+  {
+    "revision": "8f33f789c45ec339b117",
+    "url": "/ironsworn-asset-workbench/static/js/2.31e04cf3.chunk.js"
+  },
+  {
+    "revision": "6dcfec103c0715605dfbaf5f4ff156bc",
+    "url": "/ironsworn-asset-workbench/static/js/2.31e04cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0650293597c212aeb4",
+    "url": "/ironsworn-asset-workbench/static/js/main.80f781e5.chunk.js"
+  },
+  {
+    "revision": "8e0afc15f46bbab9dc85",
+    "url": "/ironsworn-asset-workbench/static/js/runtime-main.80ecfe44.js"
+  }
+]);
