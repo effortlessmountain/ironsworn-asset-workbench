8b1f4ac37a1b7737a94a6ab276e5d4061cf2edc8
Effortless Mountain <effortlessmountain@gmail.com>
2020-04-20 15:08:50 -0700

    add button for downloading directly without previewing

--- src/download.tsx
+++ src/download.tsx
@@ -1,11 +1,8 @@
 import { showAssetIn } from './asset'
-import { assetScale } from './assetScaling'
 import { showScreen } from './router'
 import { currentAsset } from './editor'
 import html2canvas from 'html2canvas'
 
-
-// TODO: implement as 'Download' and make current 'Download' into 'Preview'
 const saveImage = (uri, filename) => {
     const link = document.createElement('a')
     link.href = uri
@@ -17,35 +14,39 @@ const saveImage = (uri, filename) => {
 
 const downloadContainer = document.querySelector(".image-container")
 
-const screenshot = (asset) => {
+const renderOnCanvas = (asset, callback) => {
     const render = document.querySelector('.render')
     showScreen('render')
     showAssetIn(render, asset)
     window.scrollTo(0, 0)
-    //TODO: fix probably hacky casting
     html2canvas(render.firstChild as HTMLElement,
         {
             allowTaint: true
-            // width: "750",
-            // height: "1050",
-            // scale: 2
-            // windowHeight: "1080px",
-            // windowWidth: "1920px"
         }).then(canvas => {
             downloadContainer.appendChild(canvas)
-            // saveImage(canvas.toDataURL(), 'asset.png') //TODO name after asset's name
-            showScreen('download')
+            callback(canvas)
         })
 }
-
-const downloadButton: HTMLButtonElement = document.querySelector("#download")
-downloadButton.onclick = () => screenshot(currentAsset)
 
 
 const closeDownload = () => {
     downloadContainer.innerHTML = ""
     showScreen('main')
 }
+
+const previewDownloadButton: HTMLButtonElement = document.querySelector("#preview-download")
+previewDownloadButton.onclick =
+    () => renderOnCanvas(currentAsset, canvas => showScreen('preview-download'))
+
+const downloadButton: HTMLButtonElement = document.querySelector("#download")
+downloadButton.onclick = () => {
+    renderOnCanvas(currentAsset,
+        canvas => {
+            saveImage(canvas.toDataURL(), currentAsset.name + ".png")
+            closeDownload()
+        })
+}
+
 
 const closeDownloadbutton: HTMLButtonElement = document.querySelector("#done-downloading")
 closeDownloadbutton.onclick = closeDownload
