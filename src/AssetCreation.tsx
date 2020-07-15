221ea3e0b1b7cab1230eb9164a83ea2b75987b62
Effortless Mountain <effortlessmountain@gmail.com>
2020-07-15 16:40:42 -0700

    (#14) clone examples before editing them

--- src/AssetCreation.tsx
+++ src/AssetCreation.tsx
@@ -3,6 +3,7 @@ import { createBlankAsset, UnspecifiedAssetDocument } from './models/assetModels
 import { ironclad, lightbearer, caveLion } from './exampleAssets'
 import AssetChoice from './AssetChoice'
 import AssetCardButton from './AssetCardButton'
+import { cloneDeep } from 'lodash'
 
 export default function AssetCreation(props: {
     chooseAsset(asset: UnspecifiedAssetDocument),
@@ -10,13 +11,13 @@ export default function AssetCreation(props: {
 }) {
     return (
         <div className="asset-selection">
-            <h3>Create a blank asset or a copy of an example:</h3>
+            <h3>Create a blank asset or copy an example:</h3>
             <div className="asset-selection-controls">
                 {[ironclad, lightbearer, caveLion].map((example, index) => {
                     return (<AssetChoice
                         key={index}
                         asset={example as UnspecifiedAssetDocument}
-                        handleClick={() => props.chooseAsset(example as UnspecifiedAssetDocument)}></AssetChoice>)
+                        handleClick={() => props.chooseAsset(cloneDeep(example) as UnspecifiedAssetDocument)}></AssetChoice>)
                 })}
                 <AssetCardButton
                     text="Blank Asset"
