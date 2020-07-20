29e5d1c718b30c020683c789db8d56d97cd1b3f4
Effortless Mountain <effortlessmountain@gmail.com>
2020-07-19 22:46:17 -0700

    update version to 0.9.0

--- src/App.tsx
+++ src/App.tsx
@@ -149,7 +149,7 @@ export default class App extends React.Component<{}, AppState> {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h2> Ironsworn Asset Workbench v0.8.2</h2>
+                    <h2> Ironsworn Asset Workbench v0.9.0</h2>
                 </header>
                 {this.state.currentScreen === "preview-download" &&
                     <Download
