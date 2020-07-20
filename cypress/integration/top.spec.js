4fb8a65586495679e55a562cd96e5de62278d759
Effortless Mountain <effortlessmountain@gmail.com>
2020-07-19 22:44:32 -0700

    update e2e tests to run with the collection workflow

--- cypress/integration/top.spec.js
+++ cypress/integration/top.spec.js
@@ -3,7 +3,9 @@
 describe("the 'Top' editor screen", () => {
     it("can get to the Top editor screen", () => {
         cy.visit("http://localhost:3000")
-        cy.contains('your asset', { matchCase: false }).click()
+        cy.contains('add new asset', { matchCase: false }).click()
+        cy.get('.add-new-asset', { matchCase: false }).click()
+        cy.get('#show-top-view').should('have.class', 'selected')
     })
 
     it("can set the asset name", () => {
