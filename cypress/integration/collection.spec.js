235ff9e548a5fbfa8142565dec7604a8c8e6bfc4
Effortless Mountain <effortlessmountain@gmail.com>
2020-07-23 08:40:51 -0700

    make created blank assets more blank

--- cypress/integration/collection.spec.js
+++ cypress/integration/collection.spec.js
@@ -10,7 +10,7 @@ describe("managing a collection", () => {
             cy.contains('add new asset', { matchCase: false }).click()
             cy.get('.add-new-asset', { matchCase: false }).click()
             cy.get('.asset-back-button').click()
-            cy.get('.asset-selection-controls').children().eq(0).should('contain.text', "Your Asset")
+            cy.get('.asset-selection-controls').children().eq(0).should('exist')
         })
         it("can create an asset from an example", () => {
             cy.contains('add new asset', { matchCase: false }).click()
@@ -21,7 +21,7 @@ describe("managing a collection", () => {
         it("can delete assets", () => {
             cy.contains('Ironclad').click()
             cy.contains('DELETE ASSET').click()
-            cy.contains('Your Asset').click()
+            cy.get('.asset-choice').click()
             cy.contains('DELETE ASSET').click()
             cy.get('.asset-selection-controls').should('be.empty')
             // to ensure localstorage was updated. Note that moving this to another test doesn't work unless the test makes the app persist to localstorage again because Cypress clears localstorage between tests.
